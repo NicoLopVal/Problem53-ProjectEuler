40f74e1461630e4bea94918e0d2f177a419b0eba
nikil <nikil@DESKTOP-73MHGNO>
2022-01-25 19:18:02 -0500

    Add project files.

---
+++ Problem53-ProjectEuler/Program.cs
@@ -0,0 +1,28 @@
+﻿double answer = 0;
+
+for(double i = 2; i <= 100; i++)
+{
+    for(double j = 1; j < i; j++)
+    {
+        if(GetFactorial(i,j)>1000000)
+            answer++;
+    }
+}
+
+Console.WriteLine("There are {0} greater than one - million", answer);
+
+
+double GetFactorial(double num, double denom)
+{
+    double numerator = 1;
+    double denominator = 1;
+    for(double i = num; i > denom; i--)
+    {
+        numerator *= i;
+    }
+    for (double i = (num-denom); i > 0; i--)
+    {
+        denominator *= i;
+    }
+    return numerator / denominator;
+}
